e59afab5532814a3438d044631b50f9597dc4746
onionpower <chocoonion@gmail.com>
2018-08-29 21:32:42 +0300

    * a longeset increasing sequence

---
+++ longest-inc-sequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace longest_inc_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new[] { 3, -1, 4, 3, 7, -2, 5, 6 };
+            
+            var w = a.Select(e => 1).ToArray();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] >= a[j])
+                    {
+                        continue;
+                    }
+
+                    if (w[j] >= w[i] + 1)
+                    {
+                        continue;
+                    }
+
+                    w[j]++;
+                }
+            }
+            
+            Console.WriteLine(string.Join(", ", a));
+            Console.WriteLine(string.Join(", ", w));
+        }
+    }
+}
